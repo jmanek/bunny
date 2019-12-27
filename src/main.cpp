ca4cd9285a7ae84ebccf16ab2d27546568f47ed9
jmanek <jesse.manek@gmail.com>
2019-12-27 01:48:32 -0800

    bunny

---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include <Eigen/Core>
+#include <igl/readOBJ.h>
+#include <igl/writeOBJ.h>
+#include <igl/combine.h>
+#include <igl/copyleft/offset_surface.h>
+#include <igl/signed_distance.h>
+#include <igl/triangle_triangle_adjacency.h>
+#include <igl/bounding_box_diagonal.h>
+#include <iostream>
+#include <vector>
+#include <platonic_solid.h>
+
+void bunny(double offset, int cells) {
+    Eigen::MatrixXd V, SV, GV, S, CV, BV;
+    Eigen::MatrixXi F, SF, side(1, 3), CF, BF;
+    igl::readOBJ("../bunny.obj", V, F);
+    std::cout << "Vertices: " << V.rows() << std::endl;
+    std::cout << "Faces: " << F.rows() << std::endl;
+    std::cout << "Diagonal: " << igl::bounding_box_diagonal(V) << std::endl;
+    
+    
+    igl::copyleft::offset_surface(V, 
+                                  F,
+                                  offset,
+                                  cells,
+                                  igl::SIGNED_DISTANCE_TYPE_PSEUDONORMAL,
+                                  SV,
+                                  SF,
+                                  GV,
+                                  side,
+                                  S);
+
+    Eigen::Vector3d M = V.colwise().maxCoeff();
+    std::cout << "Top Vertex Original: " << M(0) << " " <<  M(1) << " " <<  M(2) << std::endl;
+    Eigen::Vector3d SM = SV.colwise().maxCoeff();
+    std::cout << "Top Vertex Offset: " << SM(0) << " " <<  SM(1) << " " <<  SM(2) << std::endl;
+    double d = (M - SM).norm();
+    std::cout << "Distance: " << d << std::endl;
+
+
+    igl::writeOBJ("bunny_offset.obj", SV, SF);
+    igl::combine<Eigen::MatrixXd,Eigen::MatrixXi>({V, SV}, {F, SF}, CV, CF);
+    igl::writeOBJ("bunny_combined.obj", CV, CF);
+    
+    std::cin.get();
+    int res = std::system("meshlab bunny_combined.obj");
+}
+
+int main(int argc, char **argv) { 
+    bunny(std::stod(argv[1]), std::stoi(argv[2]));
+}
